dc64ca0c5560211da2cfddae2cb6e84dbc51e0a6
Bojana Arsovska <bojana.arsovska@speakap.com>
2018-08-01 14:33:25 +0200

    ok

--- manifest.js
+++ manifest.js
@@ -1,22 +1,32 @@
 {
     "name": {
-        "en-US": "Bob's app"
+        "en-US": "Hello World"
     },
     "icon": "http://developer.speakap.io/world-icon.png",
-    "permissions": [],
+    "permissions": [
+        "get_profiles"
+    ],
     "entries": [
         {
             "position": "main",
-            "responsive":false,
             "devices": "all",
-            "url": " https://tutorial.hosting.speakap.io/experimental/bojana/index.html",
+            "url": "https://yourdomain.com/app/folder/helloworld.php",
             "icon": "\uf0ac",
             "label": {
                 "en-US": "Hello World"
             }
         },
-    ],
+        {
+            "position": "network-timeline-widget",
+            "url": "https://yourdomain.com/app/folder/helloworld.php",
+            "devices": "all",
+            "label": {
+                "en-US": "Hello World"
+            }
+        }
+    ]
 }
+
 // We have generated the following ID and secret:
 // ID: 29018071e8000714
 // secret: 04fd15176c3175cec8d9a41c35752e5efacf236d4e84885c6e5f2e4bca060127
