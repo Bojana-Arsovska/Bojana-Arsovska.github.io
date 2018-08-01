a7fd32bbe5e2b017f02e77e726163a9681b37f15
Bojana Arsovska <bojana.arsovska@speakap.com>
2018-08-01 13:08:57 +0200

    ok

--- server-side.php
+++ server-side.php
@@ -8,8 +8,6 @@ require 'vendor/autoload.php';
 
 $signedRequest = new \Speakap\SDK\SignedRequest('29018071e8000714', '04fd15176c3175cec8d9a41c35752e5efacf236d4e84885c6e5f2e4bca060127');
 
-use Speakap\SDK as SpeakapSDK;
-
 echo 'Bla';
 
 if (!$validator->validateSignature($_POST)) {
