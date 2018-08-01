80af038fbc1c568cd5785fd1cdb9dc4e0baf1d7c
Bojana Arsovska <bojana.arsovska@speakap.com>
2018-08-01 13:31:56 +0200

    ok

--- server-side.php
+++ server-side.php
@@ -6,7 +6,7 @@ ini_set("display_errors", 1);
 
 require 'vendor/autoload.php';
 
-use Speakap\SDK as SpeakapSDK;
+//use Speakap\SDK as SpeakapSDK;
 
 $signedRequest = new \Speakap\SDK\SignedRequest('29018071e8000714', '04fd15176c3175cec8d9a41c35752e5efacf236d4e84885c6e5f2e4bca060127');
 
