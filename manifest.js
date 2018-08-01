a4f4a5d249cebb6cd30452a031a96f1f9e2330f3
Bojana Arsovska <bojana.arsovska@speakap.com>
2018-08-01 10:10:43 +0200

    commiting

---
+++ manifest.js
@@ -0,0 +1,34 @@
+{
+    "name": {
+        "en-US": "Hello World"
+    },
+    "icon": "http://developer.speakap.io/world-icon.png",
+    "permissions": [],
+    "entries": [
+        {
+            "position": "main",
+            "responsive":false,
+            "devices": "all",
+            "url": " https://tutorial.hosting.speakap.io/experimental/bojana/index.html",
+            "icon": "\uf0ac",
+            "label": {
+                "en-US": "Hello World"
+            }
+        },
+        {         // FOR EXTERNAL POSITION , NOT THE ONE IN THE MENU
+            "position": "external",
+            "url": "https://tutorial.hosting.speakap.io/experimental/bojana/server-side.php",
+            "urlAppendNetwork": true,
+            "devices": "all",
+            "label": {
+                "en-US": "Hello World"
+            }
+        }
+    ],
+    "redirectUris": [
+        "https://bojana-arsovska.github.io/server-side.php"
+    ]
+}
+// We have generated the following ID and secret:
+// ID: 28fd61b50f000204
+// secret: 9081caf1649b2911f74fd0f158ca260f8d619fea8fcb4e41686f1b2511bb6a9a
