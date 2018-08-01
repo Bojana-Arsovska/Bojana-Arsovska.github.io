d7da0d8a2fc9ca0fa9482106711c7cd68d439f47
Bojana Arsovska <bojana.arsovska@speakap.com>
2018-08-01 14:27:04 +0200

    ok

--- server-side.php
+++ server-side.php
@@ -1,44 +1,69 @@
 <?php
 
-error_reporting(E_ALL);
+// error_reporting(E_ALL);
+//
+// ini_set("display_errors", 1);
+//
+// require 'vendor/autoload.php';
+//
+// //use Speakap\SDK as SpeakapSDK;
+//
+// $signedRequest = new \Speakap\SDK\SignedRequest('29018071e8000714', '04fd15176c3175cec8d9a41c35752e5efacf236d4e84885c6e5f2e4bca060127');
+//
+// echo 'Bla';
+//
+// if (!$signedRequest->validateSignature($_POST)) {
+//     die('Invalid signature');
+// }
+//
+// $encSignedReq = $signedRequest->getSignedRequest($_POST);
+//
+// echo <<<HTML
+// <html>
+//     <head>
+//         <title>Hello World</title>
+//         <script type="text/javascript">
+//             var Speakap = { appId: "29018071e8000714", signedRequest: "$encSignedReq" };
+//         </script>
+//         <script type="text/javascript" src="js/jquery.min.js"></script>
+//         <script type="text/javascript" src="js/speakap.js"></script>
+//     </head>
+//
+//     <body>
+//       <script type="text/javascript">
+//       Speakap.doHandshake.then(function() {
+//         Speakap.getLoggedInUser().then(function(loggedInUser) {
+//             Speakap.ajax('/users/' + loggedInUser.EID + '/').done(function(user) {
+//               document.write('<p>Hello ' + user.fullName + '!</p>');
+//             });
+//           });
+//         });
+//       </script>
+//   </body>
+// </html>
+// HTML;
 
-ini_set("display_errors", 1);
 
-require 'vendor/autoload.php';
+$signedParams = $signedRequest->getSignedParameters($_POST);
 
-//use Speakap\SDK as SpeakapSDK;
+$baseUrl = 'https://api.speakap.io/networks/' . $signedParams['networkEID'];
 
-$signedRequest = new \Speakap\SDK\SignedRequest('29018071e8000714', '04fd15176c3175cec8d9a41c35752e5efacf236d4e84885c6e5f2e4bca060127');
+$accessToken = /* App ID */ '29018071e8000714_04fd15176c3175cec8d9a41c35752e5efacf236d4e84885c6e5f2e4bca060127' /* Secret */;
 
-echo 'Bla';
+$ch = curl_init("$baseUrl/users/{$signedParams['userEID']}/");
 
-if (!$signedRequest->validateSignature($_POST)) {
-    die('Invalid signature');
-}
+curl_setopt_array($ch, array(
+    CURLOPT_HEADER => false,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => array(
+        'Accept: application/vnd.speakap.api-v1.4+json',
+        'Authorization: Bearer ' . $accessToken
+    )
+));
 
-$encSignedReq = $signedRequest->getSignedRequest($_POST);
+$response = curl_exec($ch);
+curl_close($ch);
 
-echo <<<HTML
-<html>
-    <head>
-        <title>Hello World</title>
-        <script type="text/javascript">
-            var Speakap = { appId: "29018071e8000714", signedRequest: "$encSignedReq" };
-        </script>
-        <script type="text/javascript" src="js/jquery.min.js"></script>
-        <script type="text/javascript" src="js/speakap.js"></script>
-    </head>
+$user = json_decode($response);
 
-    <body>
-      <script type="text/javascript">
-      Speakap.doHandshake.then(function() {
-        Speakap.getLoggedInUser().then(function(loggedInUser) {
-            Speakap.ajax('/users/' + loggedInUser.EID + '/').done(function(user) {
-              document.write('<p>Hello ' + user.fullName + '!</p>');
-            });
-          });
-        });
-      </script>
-  </body>
-</html>
-HTML;
+echo "<p>Hello {$user->fullName}!</p>";
