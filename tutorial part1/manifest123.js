a07b36c312058aab81ec0051cf7948f215b0b8e8
Bojana Arsovska <bojana.arsovska@speakap.com>
2018-08-02 11:22:52 +0200

    ok

---
+++ tutorial part1/manifest123.js
@@ -0,0 +1,31 @@
+{
+    "name": {
+        "en-US": "Vikings"
+    },
+    "icon": "http://developer.speakap.io/free-code-camp.png",
+    "permissions": [
+        "get_profiles"
+    ],
+    "entries": [
+        {
+            "position": "external",
+            "devices": "all",
+            "url": "https://tutorial.hosting.speakap.io/experimental/bojana/server-side.php",
+            "urlAppendNetwork": true,
+            "icon": "\f2c5",
+            "label": {
+                "en-US": "Vikings"
+            }
+        },
+        {
+            "position": "network-timeline-widget",
+            "url": "https://tutorial.hosting.speakap.io/experimental/bojana/server-side.php",
+            "devices": "all",
+            "label": {
+                "en-US": "Vikings"
+            }
+        }
+    ]
+}
+// ID: 29068821bb00005c
+// secret: 4da9392b8b5bf816d2a6d4feccce071c60a7e31b9dcfb3ac34a644779a30e29b
